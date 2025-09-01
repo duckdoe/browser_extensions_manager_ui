0feb81ae8df858ca5ba7fc5f0b0f6a7d4013247f
duckdoe <fortunefoluso@gmail.com>
2025-09-01 12:42:46 +0100

    Tailwind css theme setup

--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,14 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
+
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        "dark-gradient": "linear-gradient(180deg, #040918 0%, #091540 100%)",
+        "light-gradient": "linear-gradient(180deg, #ebf2fc 0%, #eef8f9 100%)",
+      },
+    },
+  },
+};

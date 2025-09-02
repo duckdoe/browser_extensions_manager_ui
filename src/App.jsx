4d117e8326ac9b7579dc5cfbe732c3eaa945edb6
duckdoe <fortunefoluso@gmail.com>
2025-09-02 20:23:49 +0100

    Fetching data from the data.json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Switch from "./Switch.jsx";
 import Remove from "./Remove.jsx";
 import Header from "./Header.jsx";
 import Navigation from "./Navigation.jsx";
+
 function App() {
+  const [extension, setExtension] = useState([]);
+  useEffect(() => {
+    fetch("src/data.json")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => console.log(data));
+  }, []);
   return (
     <>
       <Header />
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,10 +1,10 @@
 function Navigation() {
   return (
-    <nav>
+    <nav className="flex flex-col items-center">
       <h1 className="font-bold text-3xl text-center mt-8 text-neutral-900 mb-4">
         Extensions List
       </h1>
-      <div className="flex items-center justify-between mx-12">
+      <div className="flex items-center mx-12 w-70 justify-between">
         <button className="py-2 px-5 rounded-4xl bg-red-700 text-neutral-0">
           All
         </button>
